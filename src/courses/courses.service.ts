386084faa79ce96f8e5635fc58017c9c3cb9f831
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-11 18:45:42 -0300

    feat: add services

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do Framework NestJS',
+      description: 'Fundamentos do Framework NestJS',
+      tags: ['nodejs', 'nestjs', 'javascript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find((course: Course) => course.id === Number(id));
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
+}
