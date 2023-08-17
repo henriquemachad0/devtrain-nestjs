ce0035fc7c8810955a8cb9c1a323f27ac846beee
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-17 11:49:43 -0300

    feat: init test e2e courses

---
+++ test/courses/courses.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { CoursesModule } from '../../src/courses/courses.module';
+
+describe('Courses: /courses', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [CoursesModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it.todo('Create POST /courses');
+});
