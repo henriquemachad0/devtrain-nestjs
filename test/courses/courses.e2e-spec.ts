b9cad0f989a268a3589ac4ec364d42d4b3f3f5cf
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-17 15:32:06 -0300

    feat: test e2e endpoint create course 1

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { CoursesModule } from '../../src/courses/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateCourseDto } from '../../src/courses/dto/create-course.dto';
 
 describe('Courses: /courses', () => {
   let app: INestApplication;
+
+  const course = {
+    name: "NestJs com TyperORM",
+    description: "Criando APIS Restful com NestJs",
+    tags: ['nestjs', 'typeorm', 'nodejs', 'typescript']
+  }
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,6 +31,15 @@ describe('Courses: /courses', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
     await app.init();
   });
 
@@ -31,5 +47,10 @@ describe('Courses: /courses', () => {
     await app.close()
   })
 
-  it.todo('Create POST /courses');
+  it('Create POST /courses', () => {
+    return request(app.getHttpServer())
+      .post('/courses')
+      .send(course as CreateCourseDto)
+      .expect(HttpStatus.CREATED)
+  });
 });
